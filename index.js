2993becdc6d02b62ef8c4c3f3d15b5a76c8ff7c2
cyj19797-hue <cyj19797@gmail.com>
2026-01-07 09:12:51 +0900

    비밀 정보 보호

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express = require("express");
-
+const bodyParser = require("body-parser");
 const app = express();
+const { User } = require("./models/User");
+
+// application/x-ww-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//application/json
+app.use(bodyParser.json());
 
 const mongoose = require("mongoose");
 mongoose
@@ -11,7 +18,20 @@ mongoose
   .catch((err) => console.log(err));
 
 app.get("/", (req, res) => {
-  res.send("Hello World 안녕하세요!");
+  res.send("Hello World 안녕하세요! 새해 복 많이 받으세요");
+});
+
+app.post("/register", (req, res) => {
+  //회원 가입 할 때 필요한 정보들을 client에서 가져오면
+  //그것들을 데이터 베이스에 넣어준다.
+  const user = new User(req.body);
+
+  user.save((err, doc) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({
+      success: true,
+    });
+  });
 });
 
 app.listen(5000, () => {
